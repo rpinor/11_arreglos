c41f47b0e7e796bcc1484c092630f284ca0a634b
hugo-quispe <hordonoq@unjbg.edu.pe>
2024-07-01 10:49:47 -0500

    Añade Arreglos1.cpp al repositorio

---
+++ Arreglos1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n,S;
+    float No=0;
+    cin>>n;
+    int A[n];
+    for(int i=0;i<n;i++){
+        cin>>A[i];
+        S=S+A[i];
+        if(A[i]==0){
+            No++;
+        }
+    }
+    cout<<No/n*100;
+    return 0;
+}
