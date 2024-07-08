5f2f773f354726341a7e44215c54706078cd6eaa
rpinor <rpinor@unjbg.edu.pe>
2024-07-08 07:04:58 -0500

    Agrega arreglos3.cpp

---
+++ arreglos3.cpp
@@ -0,0 +1,38 @@
+#include "iostream"
+using namespace std;
+//FUNCION PARA ESCRIBIR TEMPERATURA MEDIA
+float TempMe (int a, int b){
+    float c;
+    c=(a+b)/2;
+    return c;
+}
+//FUNCION PRINCIPAL
+int main (){
+    int Tmax[6], Tmin[6], Menor=500, Temp;
+    for(int i=0; i<=6; i++){
+        cout<<"Ingrese MAX"<<endl;
+        cin>>Tmax[i];
+        cout<<"Ingrese MIN"<<endl;
+        cin>>Tmin[i];
+        if(Menor>Tmin[i]){
+            Menor=Tmin[i];
+        }
+    }
+    for(int i=0; i<=6; i++){
+        cout<<"La temperatura media del dia "<<i+1<<" es "<<TempMe(Tmax[i], Tmin[i])<<endl;
+    }
+    cout<<"La menor Temperatura es "<<Menor<<endl;
+    cout<<"Ingresa Temperatura a leer"<<endl;
+    cin>>Temp;
+    int No=0;
+    for(int i=0; i<=6; i++){
+        if(Temp==Tmax[i]){
+            cout<<"Dia "<<i+1;
+            No=0;
+        }
+    }
+    if (No==0){
+        cout<<"No existe"<<endl;
+    }
+    return 0;
+}
